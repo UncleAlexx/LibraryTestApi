7be226dfb5bb32c5241d2e57a092ed8c1be63b88
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:06:57 +0300

    Add Update Contracts

---
+++ BookApi.Presentation/Contracts/Book/Update/BookToUpdateView.cs
@@ -0,0 +1,13 @@
+﻿namespace BookApi.Presentation.Contracts.Book.Update;
+
+public readonly record struct BookToUpdateView()
+{
+    public required string Isbn { get; init; }
+    public required string Author { get; init; }
+    public required string Title { get; init; }
+    public required DateTime LendingDate { get; init; }
+    public required DateTime ReturnDate { get; init; }
+    public required Guid Id { get; init; }
+    public string? Genre { get; init; }
+    public string? Description { get; init; }
+}
---
+++ BookApi.Presentation/Contracts/Book/Update/UpdateResponse.cs
@@ -0,0 +1,5 @@
+﻿
+using BookApi.Presentation.Contracts.Book.Common.Bases;
+
+internal record UpdateResponse<T>(DateTime Date, string Route, T Inserted, T Deleted,  ushort Code = 200) 
+    : ResponseBase<T>(Date, Route, Code);
