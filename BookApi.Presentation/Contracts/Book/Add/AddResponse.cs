5cf89f0cccb4dada04b44140989295aa4fa57f3d
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:04:52 +0300

    Add Add command contracts

---
+++ BookApi.Presentation/Contracts/Book/Add/AddResponse.cs
@@ -0,0 +1,4 @@
+﻿
+using BookApi.Presentation.Contracts.Book.Common.Bases;
+
+internal record AddResponse<T>(DateTime Date, string Route, T Inserted, ushort Code=200) : ResponseBase<T>(Date, Route, Code);
