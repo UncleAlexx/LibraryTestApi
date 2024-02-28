34ab8d6f535ce7f80b7e1dd8ceecdf7a34a663b6
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:06:02 +0300

    Add Delete response

---
+++ BookApi.Presentation/Contracts/Book/Delete/DeleteResponse.cs
@@ -0,0 +1,5 @@
+﻿using BookApi.Presentation.Contracts.Book.Common.Bases;
+
+namespace BookApi.Presentation.Contracts.Book.Delete;
+
+internal record DeleteResponse<T>(DateTime Date, string Route, T Deleted, T Inserted, ushort Code = 200) : ResponseBase<T>(Date, Route, Code);
