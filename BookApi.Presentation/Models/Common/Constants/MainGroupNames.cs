472e41c18debefcfabc021ed1c2a1266db8ba9b1
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:01:05 +0300

    Add Common model constants

---
+++ BookApi.Presentation/Models/Common/Constants/MainGroupNames.cs
@@ -0,0 +1,14 @@
+﻿using BookApi.Presentation.Models.Common.Constants.Enums;
+using BookApi.Presentation.Models.Common.Extensions;
+using System.Collections.ObjectModel;
+
+namespace BookApi.Presentation.Models.Common.Constants;
+
+internal static class MainGroupNamesMapper
+{
+    internal static readonly ReadOnlyDictionary<MainGroupNames, string> Mapper = new(
+        new Dictionary<MainGroupNames, string>()
+        {
+            [MainGroupNames.BookMainGroup] = OpenApiConstants.BookModelMainGroup
+        });
+}
