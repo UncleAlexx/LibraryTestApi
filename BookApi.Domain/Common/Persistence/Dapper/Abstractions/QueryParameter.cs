7d42e44be571c997e1ee8b3c74bd552eb81c362c
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:51:59 +0300

    Add persistence abstractions

---
+++ BookApi.Domain/Common/Persistence/Dapper/Abstractions/QueryParameter.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace BookApi.Domain.Common.Persistence.Dapper.Abstractions;
+
+public abstract class QueryParameter<T> where T : QueryParameter<T>
+{
+    private static readonly ConstructorInfo _ctorInfo = typeof(T).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, [])!;
+
+    static QueryParameter()
+    {
+        static T InitInstance()
+        {
+            DynamicMethod f = new(string.Empty, typeof(T), []);
+            var info = f.GetILGenerator();
+            info.Emit(OpCodes.Newobj, _ctorInfo);
+            info.Emit(OpCodes.Ret);
+            return f.CreateDelegate<Func<T>>()();
+        }
+        Instance = InitInstance();
+    }
+
+    public readonly static T Instance;
+}
