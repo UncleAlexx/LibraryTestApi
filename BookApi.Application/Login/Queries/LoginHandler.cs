cf6cf22cce4faaff36b3d75c9c7e152f8f6510cf
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:35:31 +0300

    Login requests & JwtOptions

---
+++ BookApi.Application/Login/Queries/LoginHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace BookApi.Application.Login.Queries;
+
+public sealed class LoginHandler(IValidator<BookView> validator, JwtTokenFactory login) : IQueryHandler<Login, string, MessageResult<string>>
+{
+    private readonly JwtTokenFactory _login = login;
+
+    public Task<MessageResult<string>> Handle(Login request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(MessageResult<string>.Success(_login.CreateToken(request.Name, request.Password, request.Email, request.Secret)));
+    }
+}
