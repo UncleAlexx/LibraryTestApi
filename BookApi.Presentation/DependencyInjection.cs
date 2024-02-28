c376c8d9c60a9ccbd2fea32927ac4745ba326038
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 03:58:16 +0300

    Add DependencyInjection.cs

---
+++ BookApi.Presentation/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using BookApi.Presentation.Contracts.Book.Common;
+using BookApi.Presentation.Contracts.Book.Common.Mappers;
+using BookApi.Presentation.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookApi.Presentation;
+
+public static class DependencyInjection
+{
+    public static WebApplication AddPresentation(this WebApplication app, IServiceCollection services)
+    {
+        return app.AddBookCommands().AddLogin();
+    }
+    public static void AddPresentation2(this  IServiceCollection services)
+    {
+        services.AddSingleton<IBookMapper, BookMapper>();
+    }
+}
