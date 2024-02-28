3faf39b0f87ffdcd3b930753be6fe9b34b914c7e
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:49:39 +0300

    Add Dapper concrete Parameters

---
+++ BookApi.Domain/Book/Persistence/Dapper/QueriesParameters/BookIdParameter.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using BookApi.Domain.Common.Persistence.Dapper.Abstractions;
+
+namespace BookApi.Domain.Book.Persistence.Dapper.QueriesParameters;
+
+public class BookIdParameter : QueryParameter<BookIdParameter>
+{
+    public Guid? BookId { get; set; }
+
+    BookIdParameter() { }
+}
---
+++ BookApi.Domain/Book/Persistence/Dapper/QueriesParameters/IdParameter.cs
@@ -0,0 +1,10 @@
+﻿using BookApi.Domain.Common.Persistence.Dapper.Abstractions;
+
+namespace BookApi.Domain.Book.Persistence.Dapper.QueriesParameters;
+
+public sealed class IdParameter : QueryParameter<IdParameter>
+{
+    public Guid? Id { get; set; }
+
+    IdParameter() { }
+}
