1f549132448a7a9a1dc87263c1770547c628c189
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:05:32 +0300

    Add Common Contracts and Mapper

---
+++ BookApi.Presentation/Contracts/Book/Common/Bases/ResponseBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApi.Presentation.Contracts.Book.Common.Bases;
+
+internal abstract record ResponseBase<T>(DateTime Date, string Route, ushort Code = 200);
+
