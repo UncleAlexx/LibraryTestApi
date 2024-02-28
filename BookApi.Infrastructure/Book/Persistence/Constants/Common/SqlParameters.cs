844a5bb9fa6866156b80ed495aea8bfa8d8555a4
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:23:43 +0300

    Add dapper queries, UnitOfWork, Dapper Parameters

---
+++ BookApi.Infrastructure/Book/Persistence/Constants/Common/SqlParameters.cs
@@ -0,0 +1,16 @@
+﻿using BookApi.Infrastructure.Common;
+using Microsoft.Extensions.Configuration;
+
+namespace BookApi.Infrastructure.Book.Persistence.Constants.Common;
+
+internal static class SqlParameters
+{
+    private const string LibraryConnectionStringKey = "LibraryConnectionString";
+
+    /// <summary>
+    /// should call  ConnectionStringConfigDecorator.Config To initialize
+    /// </summary>
+    internal readonly static string? LibraryConnectionString;
+
+    static SqlParameters() => LibraryConnectionString = ConfigDecorator.Config?.GetConnectionString(LibraryConnectionStringKey) ?? "";
+}
---
+++ BookApi.Infrastructure/Book/Persistence/Constants/Library/SqlQueries.cs
@@ -0,0 +1,38 @@
+﻿namespace BookApi.Infrastructure.Book.Persistence.Constants.Library;
+
+internal class SqlQueries
+{
+    internal const string GetAll = """
+        Select LendingDate, [Return] as ReturnDate, Isbn, Title, Genre, Description, Author, l.BookID, l.ID as LendingId, s.ID as StockId
+        from Books.Lending l
+        join  Books.Stock s
+        on l.BookID = s.BookID;
+        """;
+
+    internal const string GetById = """
+        Select LendingDate, [Return] as ReturnDate, Isbn, Title, Genre, Description, Author, l.BookID, l.ID as LendingId, s.ID as StockId
+        from Books.Lending l
+        join  Books.Stock s
+        on l.BookID = s.BookID 
+        where l.BookID = @Id 
+        """;
+
+    internal const string GetByIsbn = """
+        Select LendingDate, [Return] as ReturnDate, Isbn, Title, Genre, Description, Author, l.BookID, l.ID as LendingId, s.ID as StockId
+        from Books.Lending l
+        join  Books.Stock s
+        on s.BookID = l.BookID 
+        where Isbn = @Isbn
+        """;
+
+    internal const string GetIdByIsbn = """
+        Select top (1) BookID
+        from Books.Stock
+        where Isbn = @Isbn
+        """;
+    internal const string GetBookId = """
+        Select top (1) ID
+        from Books.Books
+        where ID = @Id
+        """;
+}
