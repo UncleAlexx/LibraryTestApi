4dfd0016dead6078dbda20a84bdd43c0ff7b299e
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 05:01:54 +0300

    Add validation extension PredicateFactory and Validation Bounds

---
+++ BookApi.Domain/Common/Validation/Bounds.cs
@@ -0,0 +1,8 @@
+﻿namespace BookApi.Domain.Common.Validation;
+
+public readonly struct Bounds<T>(T min, T max) where T : struct
+{
+    public readonly T Max = max;
+    public readonly T Min = min;
+}
+
