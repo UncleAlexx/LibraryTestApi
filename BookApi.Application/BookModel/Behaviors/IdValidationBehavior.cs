e834f640f7238981ad21faa8af596cd22651adc1
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:35:58 +0300

    Add Behaviors

---
+++ BookApi.Application/BookModel/Behaviors/IdValidationBehavior.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApi.Application.BookModel.Behaviors;
+
+public class IdValidationBehavior<TRequest, TResponse>(IValidator<IdObject> validator) : IPipelineBehavior<TRequest, IResult<BookView>> where TRequest : IIdValidatable
+{
+    private readonly IValidator<IdObject> _validator = validator;
+
+    public async Task<IResult<BookView>> Handle(TRequest request, RequestHandlerDelegate<IResult<BookView>> next, CancellationToken cancellationToken)
+        => await (await _validator.ValidateAsync(IdObject.Create(request.Id))).FailValidationIfInvalidAsync(next);
+}
