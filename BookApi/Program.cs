4cf3620d369648e9104a0cc5669ca03af7800184
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 03:20:12 +0300

    Add program file

---
+++ BookApi/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args); 
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+builder.Configuration.AddUserSecrets<Program>();
+builder.Services.AddSecurityScheme();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.AddApplication();
+
+builder.Services.AddOptions<JwtBearerOptions>().BindConfiguration("JwtOptions").ValidateDataAnnotations();
+builder.Services.AddAuth2(builder.Services.BuildServiceProvider().GetService<IOptions<JwtBearerOptions>>());
+builder.Services.AddAuth();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddPresentation2();
+var app = builder.Build();
+app.AddPresentation(builder.Services);
+app.UseSwagger().UseSwaggerUI();
+app.Run();
