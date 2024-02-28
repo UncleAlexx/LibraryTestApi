7bb458ae5d9621e5ef5c0d4d63905f8a0d12fe26
UncleAlexx <57260716+snookersnookersnooker3@users.noreply.github.com>
2024-02-28 04:55:22 +0300

    Add Book aggregate

---
+++ BookApi.Domain/Book/Book.cs
@@ -0,0 +1,23 @@
+﻿using BookApi.Domain.Book.Entities;
+
+namespace BookApi.Domain.Book;
+
+public class Book : AggregateRoot<BookIdObject> 
+{
+    private Book(BookIdObject Id) : base(Id) { }
+
+    public Stock Stock { get; set; }
+    public Lending Lending { get; set; }
+
+    public static EntityResult<Book> Create(string isbn, string author, string description, string genre, string title,
+        DateTime lendingDate, DateTime returnDate, Guid bookId, Guid stockId, Guid lendingId)
+    {
+        var id = BookIdObject.Create(bookId);
+        var stock = Stock.Create(isbn, author, description, genre, title, id.Value, stockId);
+        stock.Entity.BookId = id;
+        var lending = Lending.CreateWithValidation(lendingDate, returnDate, id.Value, lendingId); 
+        lending.Entity.BookId = id;
+        var entity = new Book(id) { Stock = stock.Entity, Lending = lending.Entity };
+        return stock.Successful && lending.Successful? EntityResult<Book>.Success(entity) : EntityResult<Book>.Failed(entity);
+    }
+}
