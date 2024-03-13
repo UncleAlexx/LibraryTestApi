4ff4f5c8b41ae23fd96df1f5df627ed9a083c930
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 20:44:19 +0300

    Add global suppressions

---
+++ BookApi.Domain/GlobalSuppressions.cs
@@ -0,0 +1,19 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+
+
+
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Lending.BookIdObject.#ctor(System.Guid@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Lending.ReturnDateObject.#ctor(System.DateTime@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Lending.LendingDateObject.#ctor(System.DateTime@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.AuthorObject.#ctor(System.String@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.DescriptionObject.#ctor(System.String@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.GenreObject.#ctor(System.String@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.IsbnObject.#ctor(System.String@,System.Boolean@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.IdObject.#ctor(System.Guid@)")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Stock.TitleObject.#ctor(System.String@,System.Boolean@)")]
+[assembly: SuppressMessage("Style", "IDE0059:Unnecessary assignment of a value", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Common.Extensions.Emitter.EmitCall``1(System.Reflection.MethodBase,System.Type[])~``0")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Library.Domain.Book.ValueObjects.Lending.ReturnDateObject.#ctor(System.DateTime@,System.String@,System.Boolean@)")]
