76a54cc0495f4b9ed53d8d561bc2bda7c2502a44
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:39:12 +0300

    Fix global usings

--- BookApi.Application/GlobalUsings.cs
+++ BookApi.Application/GlobalUsings.cs
@@ -1,8 +1,7 @@
 ﻿global using BookView = Library.Domain.Book.Book;
 global using Library.Domain.Common.Results.Interfaces;
-global using System.ComponentModel.DataAnnotations;
+global using System.Runtime.CompilerServices;
 global using MediatR;
-global using Library.Domain.Book.Validation.Extensions;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.Extensions.DependencyInjection;
 global using FluentValidation;
@@ -17,11 +16,12 @@ global using Library.Domain.Common.Enums;
 global using Library.Domain.Common.Errors.ErrorKinds;
 global using Library.Domain.Book.Entities.Pocos;
 global using Library.Domain.Book.ValueObjects.Stock;
-global using Library.Domain.Book.Validation.Enums;
-global using System.Collections.ObjectModel;
-global using Library.Domain.Common.Validation;
 global using Library.Domain.Book.Persistence.Interfaces;
 global using Library.Application.Common.Abstractions.ValidatableRequests;
 global using Library.Application.Common.Validation.Extensions;
 global using ValidationResult = FluentValidation.Results.ValidationResult;
 global using Library.Application.Book.Validators;
+global using Library.Domain.Book.Entities;
+global using Library.Domain.Common.ValueObjects.Interfaces;
+global using Library.Domain.Book.ValueObjects.Lending;
+[assembly: InternalsVisibleTo("BookApi.Presentation")]
