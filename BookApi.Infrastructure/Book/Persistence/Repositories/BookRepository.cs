26552ce55555f00f537be544047f31e407d62e14
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:29:39 +0300

    Fix BookRepository

--- BookApi.Infrastructure/Book/Persistence/Repositories/BookRepository.cs
+++ BookApi.Infrastructure/Book/Persistence/Repositories/BookRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace Library.Infrastructure.Book.Persistence.Repositories;
+﻿using System.Diagnostics;
+
+namespace Library.Infrastructure.Book.Persistence.Repositories;
 
 public sealed class BookRepository(LibraryContext context,
     [FromKeyedServices(Databases.Library)] SqlConnection connection) : IBookRepository
@@ -8,56 +10,77 @@ public sealed class BookRepository(LibraryContext context,
 
     public async Task<IEnumerable<BookAggregate>> GetAllAsync()
     {
-        var a = await _connection.QueryAsync<BookPoco>(SqlQueries.GetAll);
-        var results = a.Select(x => BookAggregate.Create(x.Isbn, x.Author, x.Description, x.Genre, x.Title,
-            x.LendingDate, x.ReturnDate, x.BookId, x.StockId, x.LendingId)).Where(x => x.Successful).
-            Select(x => x.Entity);
-        return results;
+        var allPocos = await _connection.QueryAsync<BookPoco>(SqlQueries.GetAll);
+        var allEntities = allPocos.Select(book =>
+        {
+            var bookId = BookIdObject.Create(book.BookId);
+            return BookAggregate.Create(Stock.Create(IsbnObject.Create(book.Isbn), AuthorObject.Create(book.Author),
+            DescriptionObject.Create(book.Description!), GenreObject.Create(book.Genre!), TitleObject.Create(book.Title), bookId,
+            IdObject.Create(book.StockId)), Lending.Create(LendingDateObject.Create(book.LendingDate),
+            ReturnDateObject.Create(book.ReturnDate), bookId, IdObject.Create(book.LendingId)), bookId);
+        }).Where(result => result.Successful).Select(result => result.Entity);
+        return allEntities!;
     }
 
-    public async Task<BookAggregate?> GetByIdAsync(IdObject id)
+    public async Task<BookAggregate?> GetByIdAsync(BookIdObject id)
     {
         IdParameter.Instance.Id = id.Value;
-        var a = await _connection.QuerySingleOrDefaultAsync<BookPoco>(SqlQueries.GetById, IdParameter.Instance);
-        return  BookAggregate.Create(a.Isbn, a.Author, a.Description, a.Genre, a.Title, a.LendingDate, a.ReturnDate, 
-            a.BookId, a.StockId, a.LendingId).Entity ;
+        return await GetSingleBaseAsync<IdParameter, Guid>(IdParameter.Instance, SqlQueries.GetById);
     }
+
+    private async Task<Guid> GetIdByIsbnBaseAsync(IsbnObject isbn, string query)
+    {
+        IsbnParameter.Instance.Isbn = isbn.Value;
+        return await _connection.QuerySingleOrDefaultAsync<Guid>(query, IsbnParameter.Instance);
+    }
+
+    public async Task<Guid> GetStockIdByIsbnAsync(IsbnObject isbn) => await GetIdByIsbnBaseAsync(isbn, SqlQueries.GetStockIdByIsbn);
+
+    public async Task<Guid> GetLendingIdByIsbnAsync(IsbnObject isbn) => await GetIdByIsbnBaseAsync(isbn, SqlQueries.GetLendingIdByIsbn);
+
+    public async Task<Guid> GetIdByIsbnAsync(IsbnObject isbn) => await GetIdByIsbnBaseAsync(isbn, SqlQueries.GetIdByIsbn);
 
     public async Task<BookAggregate?> GetByIsbnAsync(IsbnObject isbn)
     {
         IsbnParameter.Instance.Isbn = isbn.Value;
-        var a = await _connection.QuerySingleOrDefaultAsync<BookPoco>(SqlQueries.GetByIsbn, IsbnParameter.Instance);
-        return BookAggregate.Create(a.Isbn, a.Author, a.Description, a.Genre, a.Title, a.LendingDate, a.ReturnDate, 
-            a.BookId, a.StockId, a.LendingId).Entity;
+        return await GetSingleBaseAsync<IsbnParameter, string>(IsbnParameter.Instance, SqlQueries.GetByIsbn);
     }
 
+    private async Task<BookAggregate?> GetSingleBaseAsync<TParameter, TValue>(TParameter isbn, string query) 
+        where TParameter : QueryParameter<TParameter, TValue>
+    {
+        if (query is null or "")
+            return null;
+        var poco = await _connection.QuerySingleOrDefaultAsync<BookPoco?>(query!, isbn);
+        if (poco is null)
+            return null;
+        var bookId = BookIdObject.Create(poco.BookId);
+        return BookAggregate.Create(Stock.Create(IsbnObject.Create(poco.Isbn), AuthorObject.Create(poco.Author),
+            DescriptionObject.Create(poco.Description!), GenreObject.Create(poco.Genre!), TitleObject.Create(poco.Title), bookId,
+            IdObject.Create(poco.StockId)), Lending.Create(LendingDateObject.Create(poco.LendingDate),
+            ReturnDateObject.Create(poco.ReturnDate), bookId, IdObject.Create(poco.LendingId)), bookId).Entity;
+    }
     public async Task<BookAggregate?> UpdateAsync(BookAggregate book)
     {
-        if (book is null or { Id : not { } })
+        if (book is null or { Stock: null} or { Lending: null} || (await GetByIsbnAsync(book!.Stock.Isbn) is null))
             return null;
-        var b = await GetByIsbnAsync(book.Stock.Isbn);
-        return _context.Update(BookAggregate.Create(book.Stock.Isbn.Value, book.Stock.Author.Value, 
-            book.Stock.Description.Value, book.Stock.Genre.Value, book.Stock.Title.Value, 
-            book.Lending.LendingDate.Value, book.Lending.Return.Value, b.Id.Value, b.Stock.Id.Value, 
-            b.Lending.Id.Value).Entity).Entity;
+        _context.Books.Update(book);
+        return book;
     }
 
     public async Task<BookAggregate?> AddAsync(BookAggregate book)
     {
-        IsbnParameter.Instance.Isbn = book.Stock.Isbn.Value;
-        if (book.Stock.Isbn.Value is null)
+        if (book is null or {Stock: null} or { Stock.Isbn: null} || (await GetByIsbnAsync(book!.Stock!.Isbn)) is not null)
             return null;
-        await _context.Books.AddAsync(book);
-        return book;
+        return (await _context.Books.AddAsync(book)).Entity;
     }
 
     public async Task<BookAggregate?> RemoveAsync(IsbnObject isbn)
     {
-        IsbnParameter.Instance.Isbn = isbn.Value;
-        var a = await _connection.QuerySingleOrDefaultAsync<BookPoco>(SqlQueries.GetByIsbn, IsbnParameter.Instance);
-        var entry = _context.Books.Remove(BookAggregate.Create(a.Isbn, a.Author, a.Description, a.Genre, a.Title, 
-            a.LendingDate, a.ReturnDate, a.BookId, a.StockId, a.LendingId).Entity);
+        var book = await GetByIsbnAsync(isbn);
+        if (book is null or { Id: null } or { Stock: null } or { Lending: null })
+            return null;
+        var entry = _context.Books.Remove(book!);
         return entry.Entity;
     }
-
 }
