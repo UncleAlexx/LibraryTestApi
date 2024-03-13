6291609f64e8b190af19bf21957fd67528b8f99e
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 22:30:52 +0300

    clean up Program.cs

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,22 +1,7 @@
-using Library.Infrastructure.Login.Library.Jwt;
-
-var builder = WebApplication.CreateBuilder(args); 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
-{
-    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-});
-
-builder.Configuration.AddUserSecrets<Program>();
-builder.Services.AddSecurityScheme();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.AddApplication();
-
-builder.Services.AddOptions<JwtBearerOptions>().BindConfiguration("JwtOptions").ValidateDataAnnotations();
-builder.Services.AddAuth2(builder.Services.BuildServiceProvider().GetService<IOptions<JwtBearerOptions>>());
-builder.Services.AddAuth();
+var builder = WebApplication.CreateBuilder(args);
+builder.AddWebApi().AddInfrastructure().AddApplication().AddPresentation();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddPresentation2();
 var app = builder.Build();
-app.AddPresentation(builder.Services);
-app.UseSwagger().UseSwaggerUI();
+app.UseAuthentication().UseAuthorization();
+app.MapEndpoints().UseSwagger().UseSwaggerUI();
 app.Run();
