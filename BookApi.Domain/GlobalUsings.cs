9e9006b4d84d9aad6f4715f131173c018a13ee1d
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 20:34:41 +0300

    fixed global usings

--- BookApi.Domain/GlobalUsings.cs
+++ BookApi.Domain/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿global using Library.Domain.Book.Validation.Enums;
-global using Library.Domain.Book.Validation.Extensions;
-global using Library.Domain.Book.ValueObjects.Lending;
+﻿global using Library.Domain.Book.ValueObjects.Lending;
 global using Library.Domain.Book.ValueObjects.Stock;
 global using Library.Domain.Common.Results.ResultsKind;
 global using System.Diagnostics.CodeAnalysis;
@@ -14,7 +12,6 @@ global using Library.Domain.Common.Persistence.Dapper.Abstractions;
 global using Library.Domain.Common.Persistence.Interfaces;
 global using Library.Domain.Common.Validation;
 global using System.Text.RegularExpressions;
-global using Library.Domain.Book.Validation.Constants;
 global using Library.Domain.Book.Entities;
 global using Library.Domain.Common.Abstractions;
 global using System.Runtime.CompilerServices;
@@ -22,3 +19,9 @@ global using System.Reflection;
 global using System.Reflection.Emit;
 global using Library.Domain.Common.Errors.Interfaces;
 global using Library.Domain.Common.Enums;
+global using  Library.Domain.Common.Extensions;
+global using Library.Domain.Common.ValueObjects.Interfaces;
+global using Library.Domain.Common.Abstractions.ValueObjects;
+global using Library.Domain.Common.Abstractions.ValueObjects.Constants;
+[assembly: InternalsVisibleTo("BookApi.Infrastructure")]
+[assembly: InternalsVisibleTo("BookApi.Application")]
