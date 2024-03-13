ba58e273b28b0c2d3b4228df2310e68f1de1478c
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:43:41 +0300

    Behaviors fix

--- BookApi.Application/BookModel/Behaviors/PersistenceBehavior.cs
+++ BookApi.Application/BookModel/Behaviors/PersistenceBehavior.cs
@@ -1,14 +1,15 @@
 ﻿namespace Library.Application.BookModel.Behaviors;
 
-public class PersistenceBehavior<T, TRequest>(IUnitOfWork unitOfWork) : IPipelineBehavior<T, IResult<BookView>> where T : ICommand<BookView, IResult<BookView>>
+internal sealed class PersistenceBehavior<T, TRequest>(IUnitOfWork unitOfWork) : 
+    IPipelineBehavior<T, IResult<BookView>> where T : ICommand<BookView, IResult<BookView>>
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
-    public async Task<IResult<BookView>> Handle(T request, RequestHandlerDelegate<IResult<BookView>> next, CancellationToken cancellationToken)
+    public async Task<IResult<BookView>> Handle(T request, RequestHandlerDelegate<IResult<BookView>> next, 
+        CancellationToken cancellationToken)
     {
-
-        var s = await next();
+        var nextResult = await next();
         await _unitOfWork.SaveChangesAsync();
-        return s;
+        return nextResult;
     }
 }
