c67b5fb380cd300df50006a3c59bdaaa6aecc3f7
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:07:01 +0300

    Change BookRepository interface

--- BookApi.Domain/Book/Persistence/Interfaces/IBookRepository.cs
+++ BookApi.Domain/Book/Persistence/Interfaces/IBookRepository.cs
@@ -3,9 +3,12 @@
 public interface IBookRepository : IRepository<Book>
 {
     Task<IEnumerable<Book>> GetAllAsync();
-    Task<Book?> GetByIdAsync(IdObject id);
+    Task<Book?> GetByIdAsync(BookIdObject id);
     Task<Book?> GetByIsbnAsync(IsbnObject isbn);
     Task<Book?> UpdateAsync(Book book);
     Task<Book?> AddAsync(Book book);
     Task<Book?> RemoveAsync(IsbnObject isbn);
+    Task<Guid> GetIdByIsbnAsync(IsbnObject isbn);
+    Task<Guid> GetStockIdByIsbnAsync(IsbnObject isbn);
+    Task<Guid> GetLendingIdByIsbnAsync(IsbnObject isbn);
 }
