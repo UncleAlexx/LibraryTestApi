9b5fef51fff81f273957e4459482501b4d634448
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:05:07 +0300

    changed Book Poco

--- BookApi.Domain/Book/Entities/Pocos/BookPoco.cs
+++ BookApi.Domain/Book/Entities/Pocos/BookPoco.cs
@@ -1,15 +1,15 @@
 ﻿namespace Library.Domain.Book.Entities.Pocos;
 
-public readonly record struct BookPoco
+public sealed record BookPoco
 {
     public required string Isbn { get; init; }
     public required string Author { get; init; }
     public required string Title { get; init; }
     public required DateTime LendingDate { get; init; }
     public required DateTime ReturnDate { get; init; }
-    public string? Genre { get; init; }
-    public string? Description { get; init; }
-    public Guid BookId {get; init; }
+    public required string? Genre { get; init; }
+    public required string? Description { get; init; }
+    public Guid BookId { get; init; }
     public Guid StockId { get; init; }
     public Guid LendingId { get; init; }
 }
