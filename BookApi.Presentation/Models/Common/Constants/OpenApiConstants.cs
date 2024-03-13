9d0a8d7db80a7b67ea6ff3413a416f40fd166b1b
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 22:15:48 +0300

    Fix OpenApiConstants

--- BookApi.Presentation/Models/Common/Constants/OpenApiConstants.cs
+++ BookApi.Presentation/Models/Common/Constants/OpenApiConstants.cs
@@ -2,7 +2,9 @@
 
 internal static class OpenApiConstants
 {
-    public const string BookModelMainGroup = nameof(Book);
+    public const string BookPrimaryGroup = nameof(Book);
+    public const string LoginPrimaryGroup = nameof(Login);
+    public const string DefaultPrimaryGroup = "DefaultPrimaryGroup";
     public const string QueriesSubGroup = "Queries";
     public const string CommandsSubGroup = "Commands";
 }
