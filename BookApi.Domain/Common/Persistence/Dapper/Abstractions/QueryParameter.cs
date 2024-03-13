b593d7fc435301c4c51f91096fe73353ae1da907
UncleAlexx <itsgdstorm@gmail.com>
2024-03-13 21:12:09 +0300

    Fix abstract QueryParameter

--- BookApi.Domain/Common/Persistence/Dapper/Abstractions/QueryParameter.cs
+++ BookApi.Domain/Common/Persistence/Dapper/Abstractions/QueryParameter.cs
@@ -1,22 +1,12 @@
 ﻿namespace Library.Domain.Common.Persistence.Dapper.Abstractions;
 
-public abstract class QueryParameter<T> where T : QueryParameter<T>
+internal abstract class QueryParameter<TParameter, TValue> where TParameter : QueryParameter<TParameter, TValue>
 {
-    private static readonly ConstructorInfo _ctorInfo = typeof(T).
+    [MaybeNull]
+    protected private TValue BackingField;
+
+    private static readonly ConstructorInfo _ctorInfo = typeof(TParameter).
         GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, [])!;
 
-    static QueryParameter()
-    {
-        static T InitInstance()
-        {
-            DynamicMethod f = new(string.Empty, typeof(T), []);
-            var info = f.GetILGenerator();
-            info.Emit(OpCodes.Newobj, _ctorInfo);
-            info.Emit(OpCodes.Ret);
-            return f.CreateDelegate<Func<T>>()();
-        }
-        Instance = InitInstance();
-    }
-
-    public readonly static T Instance;
+    public readonly static TParameter Instance = Emitter.EmitCall<Func<TParameter>>(_ctorInfo, [])!();
 }
