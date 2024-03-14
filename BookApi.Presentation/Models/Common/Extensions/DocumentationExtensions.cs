695052c02abfc8705f5142dde4eb3bec615936d8
UncleAlexx <itsgdstorm@gmail.com>
2024-03-14 17:48:18 +0300

    Add DocumentationExtenstions.cs

---
+++ BookApi.Presentation/Models/Common/Extensions/DocumentationExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Presentation.Models.Common.Extensions;
+
+internal static class DocumentationExtensions
+{
+    public static void ConfigureDocumentation<TType>(this RouteHandlerBuilder endpoint, ushort[] codes,
+        string defaultContentType = "application/problem+json")
+    {
+        foreach (var code in codes.AsSpan())
+        {
+            endpoint.Produces<TType>(code, defaultContentType);
+        }
+    }
+}
