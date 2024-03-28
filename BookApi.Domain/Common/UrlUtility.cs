8f2dbd7a6ef5b40dede2165e2cbb30b3dc62eace
UncleAlexx <itsgdstorm@gmail.com>
2024-03-28 16:31:58 +0300

    Vectorize UrlUtility.cs

--- BookApi.Domain/Common/UrlUtility.cs
+++ BookApi.Domain/Common/UrlUtility.cs
@@ -3,40 +3,27 @@
 public unsafe static partial class UrlUtility
 {
     private const char Slash = '/';
-    private readonly static SearchValues<char> _slashSearch = SearchValues.Create(stackalloc char[] { Slash });
+    private readonly static SearchValues<char> _slashSearch = SearchValues.Create(stackalloc [] { Slash });
     private readonly static char[] _slashReplacements = ['%', '2', 'F'];
     private const int CharSize = sizeof(char);
     private const int IntSize = sizeof(int);
-    private static void* _foundindexesPtr;
-    private static char* _replacedPtr;
 
-    public static string Encode(ReadOnlySpan<char> source)
+    [SkipLocalsInit, MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Encode(in ReadOnlySpan<char> source)
     {
-        var replaced = source.Replace(_slashReplacements.AsSpan());
-        string rawReplaced = replaced.ToString();
-        NativeMemory.Free(_replacedPtr);
-        return rawReplaced;
-    }
-
-    private static Span<char> Replace(this ReadOnlySpan<char> source,
-        Span<char> newChunk)
-    {
-        if (source.Length < 1)
-            return [];
-        if (source.IndexOfAny(_slashSearch) is -1 || Slash == newChunk[0])
-            return MemoryMarshal.CreateSpan(ref Unsafe.AsRef(in source[0]), source.Length);
-        int destinationCount = (source.Count(Slash) - 1) * newChunk.Length + source.Length;
-
-        var replaceIndexes = FindAll(source);
-
-        _replacedPtr = (char*)NativeMemory.Alloc(nuint.CreateChecked(destinationCount) * CharSize);
-        Span<char> destination = new(_replacedPtr, destinationCount);
-        var sourcePtr = (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(source));
-        var newChunkPtr = (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(newChunk));
-        int runningIndexOffset = 0, destReplacementOffset = newChunk.Length - 1;
+        if (source.IndexOfAny(_slashSearch) is -1 || source.Length == 0)
+            return source.ToString();
+        int replaceCount = (source.Count(Slash) - 1) * _slashReplacements.Length + source.Length;
+        var replaceIndexesCount = source.Count(Slash);
+        var replaceIndexes = stackalloc int[replaceIndexesCount];
+        FindAll(source, replaceIndexes);
+        char* _replacedPtr = stackalloc char[replaceCount];
+        char* sourcePtr = (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(source));
+        var newChunkPtr = (char*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(_slashReplacements.AsSpan()));
+        int runningIndexOffset = 0, destReplacementOffset = _slashReplacements.Length - 1;
         int previousReplacementIndex = 0, sourcenumberToSkipOnMemmove = 0, sourceCountToCopyOnMemove,
-            newChunkCountToCopyOnMemove = newChunk.Length * CharSize;
-        for (int i = 0; i < replaceIndexes.Length; i++)
+            newChunkCountToCopyOnMemove = _slashReplacements.Length * CharSize;
+        for (int i = 0; i < replaceIndexesCount; i++)
         {
             sourceCountToCopyOnMemove = (replaceIndexes[i] - previousReplacementIndex) * CharSize;
             Buffer.MemoryCopy(sourcePtr + (previousReplacementIndex + sourcenumberToSkipOnMemmove), _replacedPtr +
@@ -49,25 +36,31 @@ public unsafe static partial class UrlUtility
                 sourcenumberToSkipOnMemmove = 1;
             previousReplacementIndex = replaceIndexes[i];
         }
-        NativeMemory.Free(_foundindexesPtr);
-        return destination;
+        return new string(_replacedPtr, 0, replaceCount);
     }
 
-    public static ReadOnlySpan<int> FindAll(scoped in ReadOnlySpan<char> source)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void FindAll(in ReadOnlySpan<char> source, int* foundedIndexesPtr)
     {
-        int sourceCount = source.Count(Slash);
-        _foundindexesPtr = NativeMemory.Alloc(nuint.CreateChecked(sourceCount) * IntSize);
-        Span<int> foundIndexes = new(_foundindexesPtr, sourceCount);
-        foundIndexes[0] = source.IndexOfAny(_slashSearch);
-        var temp = 1;
-        for (int sourceIndex = foundIndexes[0] + 1; sourceIndex < source.Length; sourceIndex++)
+        var sourcePtr = (short*)Unsafe.AsPointer(ref MemoryMarshal.GetReference(source));
+        foundedIndexesPtr[0] = source.IndexOfAny(_slashSearch);
+        int foundedIndexesIdx = 1;
+        var slashVector = new Vector<short>(47);
+        int shortVectorCount = Vector<short>.Count;
+        for (int sourceIndex = foundedIndexesPtr[0] + 1; sourceIndex < source.Length; sourceIndex += shortVectorCount)
         {
-            if (source[sourceIndex] == Slash)
+            var vector = Vector.Load(sourcePtr + sourceIndex);
+            if (Vector.EqualsAny(vector, slashVector))
             {
-                foundIndexes[temp] = sourceIndex;
-                temp++;
+                for (int vectorIdx = 0; vectorIdx < shortVectorCount; vectorIdx++)
+                {
+                    if (vector.GetElement(vectorIdx) == '/')
+                    {
+                        foundedIndexesPtr[foundedIndexesIdx] = sourceIndex + vectorIdx;
+                        foundedIndexesIdx++;
+                    }
+                }
             }
         }
-        return foundIndexes;
     }
 }
